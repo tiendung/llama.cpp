57d6b48287ad20001406a799aad8502b6fe10881
Tien Dung <dungtn@gmail.com>
2023-03-20 19:10:52 +0700

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-./main -m ggml-model-q4_0.bin -n 128 --repeat_penalty 1.0 --color -i -r "Tuoc: " -p \
+./main -m models/7B/ggml-model-q4_0.bin -n 128 --repeat_penalty 1.0 --color -i -r "Tuoc: " -p \
 "Transcript of a dialog, where the Tuoc interacts with an Assistant named Nam. Nam is helpful, kind, honest, good at writing, and never fails to answer the Tuoc's requests immediately and with precision.
 
 Tuoc: Hello, Nam.
@@ -7,4 +7,6 @@ Tuoc: Please tell me which city is the capital of Vietnam.
 Nam: Sure. The capital of Vietnam is Hanoi.
 Tuoc: "
 
-# scp -P 2233 quenn@118.70.171.68:~/snap/kissnap/llama.cpp/ggml-model-q4_0.bin.7z .
+# python3 convert-pth-to-ggml.py models/7B/ 1
+
+# python3 quantize.py 7B
